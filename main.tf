da3c8cd9e54b5d15934d4f6e5edd25e97e89b49f
Matthew Davis <matthew@matthewdavis.io>
2021-07-06 17:48:55 -0400

    bump

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ provider "kubernetes" {
     host                   = var.host
     token                  = var.token
     cluster_ca_certificate = var.cluster_ca_certificate
-    insecure               = true
+    insecure               = var.insecure
 
 }
 
